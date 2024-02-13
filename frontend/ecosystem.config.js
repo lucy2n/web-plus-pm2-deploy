f527f718356d272f799c8181a0c7f123de1287e6
lucy2n <lyusianaumenko@yandex.by>
2024-02-13 15:15:19 +0300

    feat: add ecosystem.config

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPO = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'service',
+    script: "./build/index.html",
+  }],
+
+  // Настройка деплоя
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      "post-deploy": "cd frontend && npm i && npm run build",
+    },
+  },
+};
