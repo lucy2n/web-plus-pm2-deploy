9aaabac3f63ae88c81bea5b33e04cd1a7338cc87
Lucy Naumenko <117926370+lucy2n@users.noreply.github.com>
2024-02-14 17:50:05 +0300

    Update ecosystem.config.js

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -19,7 +19,7 @@ module.exports = {
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
       'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      'post-deploy': 'npm i && npm run build',
+      'post-deploy': 'cd frontend && pwd && npm ci && npm run build',
     },
   },
 };
