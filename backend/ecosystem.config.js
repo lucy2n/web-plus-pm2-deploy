6d44c2cb12e24697eebd9933f6dd8f46b3aa9b57
lucy2n <lyusianaumenko@yandex.by>
2024-02-14 18:35:41 +0300

    fix remarks

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -20,6 +20,7 @@ module.exports = {
       path: DEPLOY_PATH,
       'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
       'post-deploy': 'cd ~/web-plus-pm2-deploy/backend/ && sudo npm i && npm run build && pm2 start',
+
     },
   },
 };
