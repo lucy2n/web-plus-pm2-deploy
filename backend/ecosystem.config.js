6b84e51854474ef8d385896389c312278fdb24b5
lucy2n <lyusianaumenko@yandex.by>
2024-02-17 12:16:55 +0300

    feat: fix ecosystem for backend and frontend

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,6 +1,6 @@
 const dotenv = require('dotenv');
 
-dotenv.config({ path: './env.deploy' });
+dotenv.config({ path: './.env.deploy' });
 
 const {
   DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPO,
